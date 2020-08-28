afca3d2e3f7b04d56e49449397f7a4e055314604
JieunZoeyOh <ohjieun@kakao.com>
2020-08-28 15:45:20 +0900

    /* change promise to async_await

--- ch11async/callback-to-promise.js
+++ ch11async/callback-to-promise.js
@@ -29,12 +29,21 @@ class UserStorage {
     });
   }
 }
-
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
+
 userStorage
   .loginUser(id, password)
   .then(userStorage.getRoles)
   .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`))
   .catch(console.log);
+
+// ======== async await로 바꾸기
+async function asyncUser(id, password) {
+  const user = await userStorage.loginUser(id, password);
+  const userRole = await userStorage.getRoles(user);
+  return `Hello ${userRole.name}, you have a ${userRole.role} role`;
+}
+
+asyncUser(id, password).then(console.log).catch(console.log);
