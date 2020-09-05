7e340dd68c035d1e97c038781cf33bb996ecf603
JieunZoeyOh <ohjieun@kakao.com>
2020-09-05 23:23:27 +0900

    /* made a clock with setInterval & a function Saving the User Name

---
+++ JSBasic/greeting.js
@@ -0,0 +1,38 @@
+'use strict';
+const form = document.querySelector('.js-form'),
+  input = form.querySelector('input'),
+  greeting = document.querySelector('.js-greetings');
+
+const USER_LS = 'currentUser',
+  SHOWING_CN = 'showing';
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function askeForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener('submit', handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `Hello ${text}`;
+}
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askeForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+function init() {
+  loadName();
+}
+init();
